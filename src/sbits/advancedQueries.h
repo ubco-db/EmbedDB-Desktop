3b5e5457f12a004681002be4476f74366bb9c91d
justino599 <justin.schoenit@gmail.com>
2023-07-19 16:46:00 -0700

    Add operator struct

--- src/sbits/advancedQueries.h
+++ src/sbits/advancedQueries.h
@@ -3,17 +3,6 @@
 
 #include "sbits.h"
 
-/**
- * @brief	Execute a function on each return of an iterator
- * @param	state	The sbitsState struct with the database to query
- * @param	it		An iterator that has been initialized to query the relevant data
- * @param	func	The function to run on each (key, data) pair returned by the iterator. Should return 0 or 1 to indicate whether exec should return the pair.
- * @param	key		Pre-allocated space for the key
- * @param	data	Pre-allocated space for the data
- * @return	1 if another (key, data) pair was returned, 0 if there are no more pairs to return
- */
-int8_t exec(sbitsState* state, sbitsIterator* it, int8_t (*func)(void* key, void* data), void* key, void* data);
-
 typedef struct {
     /**
      * @brief	Resets the state
@@ -38,6 +27,24 @@ typedef struct {
      */
     void* state;
 } sbitsAggrOp;
+
+typedef struct sbitsOperator_ {
+    struct sbitsOperator_* input;
+    int8_t (*func)(sbitsState* state, void* info, void* key, void* data);
+    void* info;
+} sbitsOperator;
+
+/**
+ * @brief	Execute a function on each return of an iterator
+ * @param	state	The sbitsState struct with the database to query
+ * @param	it		An iterator that has been initialized to query the relevant data
+ * @param	func	The function to run on each (key, data) pair returned by the iterator. Should return 0 or 1 to indicate whether exec should return the pair.
+ * @param	key		Pre-allocated space for the key
+ * @param	data	Pre-allocated space for the data
+ * @return	1 if another (key, data) pair was returned, 0 if there are no more pairs to return
+ */
+// int8_t exec(sbitsState* state, sbitsIterator* it, int8_t (*func)(void* key, void* data), void* key, void* data);
+int8_t exec(sbitsState* state, sbitsOperator* operator, void * key, void* data);
 
 /**
  * @brief	Calculate an aggregate function over specified groups
