d453415dffe1f48662f0143e8c32e876c58e8b36
Gerren Hunter <44909431+CookiedOutMonster@users.noreply.github.com>
2023-11-21 17:47:44 -0800

    Update src/embedDB/embedDB.c

--- src/embedDB/embedDB.c
+++ src/embedDB/embedDB.c
@@ -1284,7 +1284,7 @@ void embedDBInitIterator(embedDBState *state, embedDBIterator *it) {
         if (RADIX_BITS > 0) {
             radixsplineFind(state->rdix, it->minKey, state->compareKey, &location, &lowbound, &highbound);
         } else {
-            splineFind(state->spl, it->minKey, state->compareKey, &location, &lowbound, &highbound);  //
+            splineFind(state->spl, it->minKey, state->compareKey, &location, &lowbound, &highbound);  
         }
 
         // Use the low bound as the start for our search
