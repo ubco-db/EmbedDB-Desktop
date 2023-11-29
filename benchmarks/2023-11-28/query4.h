de472ee8be26d692a2a9e104d27b54e85cb51ed2
justino599 <justin.schoenit@gmail.com>
2023-11-28 18:51:53 -0800

    Query 1

---
+++ benchmarks/2023-11-28/query4.h
@@ -0,0 +1,9 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../src/embedDB/embedDB.h"
+#include "../../src/query-interface/advancedQueries.h"
+
+void execOperatorQuery4(embedDBState* state);
