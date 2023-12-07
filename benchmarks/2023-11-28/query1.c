73f02e803a30a6bb6becb130d96a85a8db13827a
justino599 <justin.schoenit@gmail.com>
2023-12-07 15:41:43 -0800

    Update with code not using floor

--- benchmarks/2023-11-28/query1.c
+++ benchmarks/2023-11-28/query1.c
@@ -1,24 +1,19 @@
 #include "query1.h"
 
-int embedDBFloor(double x) {
-    int xi = (int)x;
-    return x < xi ? xi - 1 : xi;
-}
-
 int8_t groupFunction(const void* lastRecord, const void* record) {
     uint32_t lastValue = *((uint32_t*)((int8_t*)lastRecord + 0));
     uint32_t value = *((uint32_t*)((int8_t*)record + 0));
-    return embedDBFloor((lastValue / 86400.0)) == embedDBFloor((value / 86400.0));
+    return (lastValue / 86400) == (value / 86400);
 }
 
 void customAggregateFunc0(embedDBAggregateFunc* aggFunc, embedDBSchema* schema, void* recordBuffer, const void* lastRecord) {
     uint32_t lastValue = *((uint32_t*)((int8_t*)lastRecord + 0));
-    uint32_t calculatedValue = embedDBFloor((lastValue / 86400.0));
+    uint32_t calculatedValue = (lastValue / 86400);
     memcpy((int8_t*)recordBuffer + getColOffsetFromSchema(schema, aggFunc->colNum), &calculatedValue, sizeof(uint32_t));
 }
 
 embedDBOperator* createOperator(embedDBState* state, void*** allocatedValues) {
-    embedDBIterator* it = malloc(sizeof(embedDBIterator));
+    embedDBIterator* it = (embedDBIterator*)malloc(sizeof(embedDBIterator));
     it->minKey = NULL;
     it->maxKey = NULL;
     it->minData = NULL;
@@ -30,13 +25,13 @@ embedDBOperator* createOperator(embedDBState* state, void*** allocatedValues) {
     int8_t colSignedness[] = {embedDB_COLUMN_UNSIGNED, embedDB_COLUMN_SIGNED, embedDB_COLUMN_SIGNED, embedDB_COLUMN_SIGNED};
     embedDBSchema* schema = embedDBCreateSchema(numCols, colSizes, colSignedness);
     embedDBOperator* scanOp = createTableScanOperator(state, it, schema);
-    embedDBAggregateFunc* group = calloc(1, sizeof(embedDBAggregateFunc));
+    embedDBAggregateFunc* group = (embedDBAggregateFunc*)calloc(1, sizeof(embedDBAggregateFunc));
     group->compute = customAggregateFunc0;
     group->colSize = 4;
     embedDBAggregateFunc* MINC2 = createMinAggregate(1, -4);
     embedDBAggregateFunc* MAXC3 = createMaxAggregate(1, -4);
     embedDBAggregateFunc* avg3 = createAvgAggregate(1, 4);
-    embedDBAggregateFunc* aggFuncs = malloc(4 * sizeof(embedDBAggregateFunc));
+    embedDBAggregateFunc* aggFuncs = (embedDBAggregateFunc*)malloc(4 * sizeof(embedDBAggregateFunc));
     aggFuncs[0] = *group;
     aggFuncs[1] = *MINC2;
     aggFuncs[2] = *MAXC3;
@@ -50,7 +45,7 @@ embedDBOperator* createOperator(embedDBState* state, void*** allocatedValues) {
     free(MAXC3);
     free(avg3);
 
-    *allocatedValues = malloc(2 * sizeof(void*));
+    *allocatedValues = (void**)malloc(2 * sizeof(void*));
     ((void**)*allocatedValues)[0] = it;
     ((void**)*allocatedValues)[1] = aggFuncs;
 
