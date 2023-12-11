32f6ad7af866d93c8363617d61f439d5823ec0ea
CookiedOutMonster <gerren.14@gmail.com>
2023-12-10 16:29:58 -0800

    finished tests

--- test/Test_var_data_buffered_read.c
+++ test/Test_var_data_buffered_read.c
@@ -70,7 +70,7 @@ void tearDown() {
     free(state->fileInterface);
     free(state);
     state = NULL;
-    //inserted = 0;
+    inserted = 0;
 }
 
 void test_insert_single_record_and_retrieval_from_buffer_no_flush(void) {
@@ -79,7 +79,7 @@ void test_insert_single_record_and_retrieval_from_buffer_no_flush(void) {
     char varData[] = "Hello world";  // size 12
     embedDBPutVar(state, &key, &data, varData, 12);
     //  reset data
-    uint32_t expData[] = {0,0,0};
+    uint32_t expData[] = {0, 0, 0};
     // create var data stream
     embedDBVarDataStream *varStream = NULL;
     // create buffer for input
@@ -94,11 +94,11 @@ void test_insert_single_record_and_retrieval_from_buffer_no_flush(void) {
     TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(varData, varDataBuffer, 12, "embedDBGetVar did not return the correct vardata");
 }
 
-void test_single_variable_page_insert_and_retrieve_from_buffer(void){
+void test_single_variable_page_insert_and_retrieve_from_buffer(void) {
     // insert just over a page worth's of data
     insertRecords(27);
     int key = 26;
-    uint32_t expData[] = {0,0,0};
+    uint32_t expData[] = {0, 0, 0};
     // create var data stream
     embedDBVarDataStream *varStream = NULL;
     // create buffer for input
@@ -113,39 +113,51 @@ void test_single_variable_page_insert_and_retrieve_from_buffer(void){
     TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(varData, varDataBuffer, 15, "embedDBGetVar did not return the correct vardata");
 }
 
-/*
-void test_insert_retrieve_insert_and_retrieve_again(void){
-    // insert 3 records 
+void test_insert_retrieve_insert_and_retrieve_again(void) {
+    // insert 3 records
     insertRecords(3);
-    // retrieve record 
-    int key = 2; 
-    uint32_t expData[] = {0,0,0};
-     // create var data stream
+    // retrieve record
+    int key = 2;
+    uint32_t expData[] = {0, 0, 0};
+    // create var data stream
     embedDBVarDataStream *varStream = NULL;
     // create buffer for input
     uint32_t varBufSize = 15;  // Choose any size
     void *varDataBuffer = malloc(varBufSize);
     // query embedDB
     int r = embedDBGetVar(state, &key, &expData, &varStream);
+    // test that records are found
     TEST_ASSERT_EQUAL_INT_MESSAGE(r, 0, "Records should have been found.");
+    // retrieve variable record
     uint32_t bytesRead = embedDBVarDataStreamRead(state, varStream, varDataBuffer, varBufSize);
+    // create string to compaare to
     char varData[] = "Testing 002...";
+    // test
     TEST_ASSERT_EQUAL_UINT32_MESSAGE(15, bytesRead, "Returned vardata was not the right length");
     TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(varData, varDataBuffer, 15, "embedDBGetVar did not return the correct vardata");
     free(varDataBuffer);
+    // insert more records
     insertRecords(60);
-    key = 25;
+    key = 55;
+    // create buffer for input
     varDataBuffer = malloc(varBufSize);
     r = embedDBGetVar(state, &key, &expData, &varStream);
-    TEST_ASSERT_EQUAL_INT_MESSAGE(r, 0, "Records should have been found.");
-}*/
+    // test that records are found
+    TEST_ASSERT_EQUAL_INT_MESSAGE(0, r, "Records should have been found.");
+    char varData_2[] = "Testing 055...";
+    // retrieve variable record
+    bytesRead = embedDBVarDataStreamRead(state, varStream, varDataBuffer, varBufSize);
+    // test
+    TEST_ASSERT_EQUAL_UINT32_MESSAGE(15, bytesRead, "Returned vardata was not the right length");
+    TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(varData_2, varDataBuffer, 15, "embedDBGetVar did not return the correct vardata");
+}
 
-void test_var_read_iterator_buffer(void){
-    insertRecords(3);
-    printf("next datapage id = %d\n", state->nextDataPageId);
+void test_var_read_iterator_buffer(void) {
+    insertRecords(5);
     embedDBIterator it;
-    uint32_t *itKey, *itData;
-    uint32_t minKey = 0, maxKey = 2;
+    uint32_t *itKey;
+    uint32_t itData[] = {0, 0, 0};
+    uint32_t minKey = 0, maxKey = 3;
     it.minKey = &minKey;
     it.maxKey = &maxKey;
     it.minData = NULL;
@@ -157,30 +169,27 @@ void test_var_read_iterator_buffer(void){
 
     embedDBInitIterator(state, &it);
 
-    while (embedDBNextVar(state, &it, &itKey, itData, &varStream)) {
-        printf("This was never called\n");
-        /* Process fixed part of record */
-        /* Process vardata if this record has it */
+    char varData[] = "Testing 000...";
+    int temp = 0;
+
+    while (embedDBNextVar(state, &it, &itKey, (void **)&itKey, &varStream)) {
         if (varStream != NULL) {
             uint32_t numBytesRead = 0;
             while ((numBytesRead = embedDBVarDataStreamRead(state, varStream, varDataBuffer, varBufSize)) > 0) {
-                /* Process the data read into the buffer */
-                for (uint32_t i = 0; i < numBytesRead; i++) {
-                    printf("%02X ", (uint8_t*)varDataBuffer + i);
-                }
             }
-            printf("\n");
+            TEST_ASSERT_EQUAL_CHAR_ARRAY_MESSAGE(varData, varDataBuffer, 15, "embedDBGetVar did not return the correct vardata");
+            // modify varData according to eaach record.
+            temp += 1;
+            char whichRec = temp + '0';
+            varData[10] = whichRec;
 
             free(varStream);
             varStream = NULL;
         }
     }
-
     free(varDataBuffer);
     embedDBCloseIterator(&it);
-
 }
-
 
 int insertRecords(uint32_t n) {
     char varData[] = "Testing 000...";
@@ -195,19 +204,16 @@ int insertRecords(uint32_t n) {
         if (result != 0) {
             return result;
         }
+        inserted++;
     }
     return 0;
-    inserted++;
 }
-
-// @TODO I want to know what happens with these cases: 
-// - If the variable record takes up multiple pages 
 
 int main() {
     UNITY_BEGIN();
-    //RUN_TEST(test_insert_single_record_and_retrieval_from_buffer_no_flush);
-    //RUN_TEST(test_single_variable_page_insert_and_retrieve_from_buffer);
-    //RUN_TEST(test_insert_retrieve_insert_and_retrieve_again);
+    RUN_TEST(test_insert_single_record_and_retrieval_from_buffer_no_flush);
+    RUN_TEST(test_single_variable_page_insert_and_retrieve_from_buffer);
+    RUN_TEST(test_insert_retrieve_insert_and_retrieve_again);
     RUN_TEST(test_var_read_iterator_buffer);
     UNITY_END();
 }
